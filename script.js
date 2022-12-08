0cefac3e9c993c1f1022e48f02251ca91ba2bee4
Gabriel Conceição <gabrielc2019@outlook.com.br>
2022-12-08 12:23:02 -0300

    limpando o código

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-//const hex = num=> parseInt(num).toString(16);
-
 function hex(num){
     let hex = parseInt(num).toString(16);
     
@@ -7,41 +5,24 @@ function hex(num){
     return hex;
 }
 
-
 const input = document.querySelectorAll('input');
-const [inputR, inputG, inputB] = input;
 const hexdec = document.querySelector('#hex');
 const rgb = document.querySelector('#rgb');;
 const viewer = document.querySelector('#viewer');
 const button = document.querySelector('button');
 
-const rgbCode = () =>  `${inputR.value},${inputG.value},${inputB.value}`;
+const rgbCode = () =>  `${input[0].value},${input[1].value},${input[2].value}`;
 
-const hexCode = ()=> `#${hex(inputR.value)}${hex(inputG.value)}${hex(inputB.value)}`;
+const hexCode = ()=> `#${hex(input[0].value)}${hex(input[1].value)}${hex(input[2].value)}`;
 
-inputR.addEventListener('input', ()=>{
+input.forEach(el => el.addEventListener('input', ()=>{
+    el.nextElementSibling.innerHTML = el.value;
+
     viewer.style.background = `rgb(${rgbCode()})`;
-    inputR.nextElementSibling.innerHTML = inputR.value;
     hexdec.innerHTML = hexCode();
     rgb.innerHTML = rgbCode();
-});
+}))
 
-inputG.addEventListener('input', ()=>{
-    viewer.style.background = `rgb(${rgbCode()})`;
-    inputG.nextElementSibling.innerHTML = inputG.value;
-
-    hexdec.innerHTML = hexCode();
-    rgb.innerHTML = rgbCode();;
-});
-
-inputB.addEventListener('input', ()=>{
-    viewer.style.background = `rgb(${rgbCode()})`;
-    inputB.nextElementSibling.innerHTML = inputB.value;
-
-    hexdec.innerHTML =  hexCode();
-    rgb.innerHTML = rgbCode();;
-});
- 
 
 hexdec.innerHTML = hexCode();
 rgb.innerHTML = rgbCode();
